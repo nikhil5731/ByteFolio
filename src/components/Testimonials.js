bc15974ffe8a34be4adce615c3a716bdd2dca60e
Nikhil Kumar <105017554+nikhil5731@users.noreply.github.com>
2026-05-05 14:36:12 +0530

    Add useCallback to Testimonials component

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { FaQuoteLeft, FaStar, FaChevronLeft, FaChevronRight, FaUserCircle } from 'react-icons/fa';
 import './Testimonials.css';
 
